9f456b04e712474c0606a3bc1938ce31e5840b96
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-05 16:48:15 -0500

    Section 14 progress

--- Full_Stack/14 String Concatenation/Section_14.js
+++ Full_Stack/14 String Concatenation/Section_14.js
@@ -1,11 +1,17 @@
-var message = "Hello";
-var name = "Chris";
+// var message = "Hello";
+// var name = "Chris";
 
 // alert(message + ' there' + ' ' + name);
 
-console.log(name.length);
+// console.log(name.length);
+
+
+// var tweet = prompt('Complete your tweet:');
+// var tweeetCount = tweet.length;
+// alert("You have wrriten " + tweeetCount + " characters, you have " + (140 - tweeetCount) + " characters remaining.");
 
 
 var tweet = prompt('Complete your tweet:');
-var tweeetCount = tweet.length;
-alert("You have wrriten " + tweeetCount + " characters, you have " + (140 - tweeetCount) + " characters remaining.");
+var tweetUnder140 = tweet.slice(0,140);
+console.log(tweetUnder140);
+
