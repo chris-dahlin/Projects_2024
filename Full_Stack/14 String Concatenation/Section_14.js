bf4ecaff41ed72195c76460979c72cf408d07132
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-05 14:38:35 -0500

    Section 14 progress

---
+++ Full_Stack/14 String Concatenation/Section_14.js
@@ -0,0 +1,11 @@
+var message = "Hello";
+var name = "Chris";
+
+// alert(message + ' there' + ' ' + name);
+
+console.log(name.length);
+
+
+var tweet = prompt('Complete your tweet:');
+var tweeetCount = tweet.length;
+alert("You have wrriten " + tweeetCount + " characters, you have " + (140 - tweeetCount) + " characters remaining.");
