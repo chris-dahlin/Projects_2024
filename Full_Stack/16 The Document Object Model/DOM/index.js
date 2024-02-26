70747b83c05f1c9256379e1a7d9f26555f321e4a
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-25 19:39:11 -0500

    Javascript update in intro to DOM section

--- Full_Stack/16 The Document Object Model/DOM/index.js
+++ Full_Stack/16 The Document Object Model/DOM/index.js
@@ -1,3 +1,3 @@
 var name = prompt("What is your name?");
 
-document.querySelector("h1").innerHTML = "Goodbye, " + name + "!";
+document.querySelector("h1").innerHTML = "How is your day going " + name + "?";
