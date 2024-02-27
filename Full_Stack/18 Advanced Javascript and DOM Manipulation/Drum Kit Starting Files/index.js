48a24bef864103f4354275c3432061348d5d874b
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-27 18:04:42 -0500

    Added eventlistener for key strokes

--- Full_Stack/18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Full_Stack/18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -52,36 +52,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
       });
     }
 
+document.addEventListener("keydown", function (event) {
+alert("key was pressed!");
+console.log(event);
 
-
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
-
-// document.querySelector('button').addEventListener('click', handleClick);
-
-// function handleClick(event) {
-//  alert('I got clicked!');
-// }
-
-
-// }
-// function add(num1, num2) {
-//   return num1 + num2;
-//   }
-   
-//   function subtract(num1, num2) {
-//   return num1 - num2;
-//   }
-   
-//   function multiply(num1, num2) {
-//   return num1 * num2;
-//   }
-   
-//   function divide(num1, num2) {
-//   return num1 / num2;
-//   }
-
-// function calculator(num1, num2, operator) {
-//     return operator(num1, num2);
-// }
-
+});
