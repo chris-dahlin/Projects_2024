46413540a6ad5754aae53935a4681fdd0d746eb3
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-26 19:00:49 -0500

    Drum set images added

--- Full_Stack/18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Full_Stack/18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,16 +1,20 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-var audio = new Audio("sounds/tom-1.mp3");
+// var audio = new Audio("sounds/tom-1.mp3");
 
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        audio.play();
+
+     
       });
     }
 
 
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
 
 // document.querySelector('button').addEventListener('click', handleClick);
 
