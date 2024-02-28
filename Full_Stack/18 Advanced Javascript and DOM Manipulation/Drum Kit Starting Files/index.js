896b022d6f633ca2cac292fa886642f16fd13919
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-28 17:26:31 -0500

    Added css for buttons pressed

--- Full_Stack/18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Full_Stack/18 Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -11,6 +11,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
       var buttonInnerHTML = this.innerHTML;
 
       makeSound(buttonInnerHTML);
+
+      buttonAnimation(buttonInnerHTML);
       });
     }
 
@@ -21,6 +23,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 document.addEventListener("keydown", function(event) {
   
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 
 });
 
@@ -67,3 +71,18 @@ function makeSound(key) {
       
   }
 }
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
+
+
