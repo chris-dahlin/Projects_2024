fa91b540db0089153b0ffd428dc7a1117c00359d
Chris Dahlin <chrisdahlin@gmail.com>
2024-02-29 19:57:47 -0500

    jQuery Progress

--- Full_Stack/19 jQuery/index.js
+++ Full_Stack/19 jQuery/index.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-$("h1").css("color", "red"); 
+// $("h1").addClass("big-title margin-50");
+
+$("button").html("<em>Click me!</em>");
+
+console.log($("img").attr("src"));
+
+$("a").attr("href", "https://www.youtube.com");
+
+
+$('h1').click(function() {
+$('h1').css('color', 'purple');
 });
