63f8c3c3930483a122fa563c2e36701001a5237e
Chris Dahlin <chrisdahlin@gmail.com>
2024-03-04 17:32:48 -0500

    Step 1 & 2 Complete

--- Full_Stack/20 Boss Level Challenge - The Simon Game/Simon Game Challenge Starting Files/game.js
+++ Full_Stack/20 Boss Level Challenge - The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,14 @@
-alert("Test");
+var buttonColours = ["red", "blue", "green", "yellow"]; 
+
+var gamePattern = [];
+
+randomChosenCoolour = buttonColours[Math.floor(Math.random() * 4)];
+
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+}
