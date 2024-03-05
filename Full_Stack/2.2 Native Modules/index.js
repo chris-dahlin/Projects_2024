2a7ebd622db9f5e119e1cdf582f9dfa9d4cd98dc
Chris Dahlin <chrisdahlin@gmail.com>
2024-03-05 14:29:44 -0500

    Node readFile

--- Full_Stack/2.2 Native Modules/index.js
+++ Full_Stack/2.2 Native Modules/index.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from NodeJS!!", (err) => {
+// fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+//   }); 
+
+  fs.readFile('./message.txt','utf-8', (err, data) => {
     if (err) throw err;
-    console.log('The file has been saved!');
+    console.log(data);
   }); 
