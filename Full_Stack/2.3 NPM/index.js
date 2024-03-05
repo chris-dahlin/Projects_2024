77347dff24625fa26a5c3b78a6d2831e1b9f9845
Chris Dahlin <chrisdahlin@gmail.com>
2024-03-05 15:15:55 -0500

    Silly Name and Super Hero name generator

--- Full_Stack/2.3 NPM/index.js
+++ Full_Stack/2.3 NPM/index.js
@@ -1,6 +1,14 @@
 // var generateName = require('sillyname');
 
-import generateName from 'sillyname';
-var sillyName = generateName();
+// import generateName from 'sillyname';
+// var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from 'superheroes';
+
+const superName = superheroes.random();
+
+// const superheroes = require('superheroes');
+
+console.log(`I am ${superName}!`);
