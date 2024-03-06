a00fd2e3cdc192c0edb05a362e8e666cec17be4d
Chris Dahlin <chrisdahlin@gmail.com>
2024-03-06 16:13:31 -0500

    Postman Basics

--- Full_Stack/Backend/3.3 Postman/index.js
+++ Full_Stack/Backend/3.3 Postman/index.js
@@ -18,15 +18,15 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/chris", (req, res) => {
   res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
+app.patch("/user/chris", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
+app.delete("/user/chris", (req, res) => {
   //Deleting
   res.sendStatus(200);
 });
